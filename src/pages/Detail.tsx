a580c2711d76decdd8d2a422c0584062374dee07
Heebeom Song <songh6508@naver.com>
2024-11-11 17:49:25 +0900

    테마 정의

---
+++ src/pages/Detail.tsx
@@ -0,0 +1,13 @@
+import Footer from "../components/common/Footer";
+import Header from "../components/common/Header";
+import Sidebar from "../components/common/Sidebar";
+
+function Detail(){
+    return (
+        <>
+            <div>Detail</div>
+        </>
+    )
+}
+
+export default Detail;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import Footer from "../components/common/Footer";
+import Header from "../components/common/Header";
+import { formatNumber } from "../utils/format";
+
+const COUNT = 10000;
+
+function Home() {
+    return(
+        <>
+            <div>book store</div>
+            <div>count: {formatNumber(COUNT)}</div>
+        </>
+        
+    )
+}
+
+export default Home;
